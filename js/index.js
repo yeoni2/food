52dd9a5d51eda575138aaa0c70f70b3407c1dd8a
yeoni2 <119648567+yeoni2@users.noreply.github.com>
2023-03-31 23:20:20 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// window.onload=function(){
+    
+// }
+// $(window).scroll(function (){
+//     var y = $(window).scrollTop();
+//     console.log(y);
+//     if(y>4000){
+//         $(".bar").css({ "position": "fixed", "top": "800px" });
+//     }
+//     else if(y>4100){
+//         $(".bar").css({ "position": "fixed", "top": "700px" });
+//     }
+//     else if(y>4200){
+//         $(".bar").css({ "position": "fixed", "top": "600px" });
+//     }
+// })
